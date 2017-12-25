ca43c9bf7d6b0b57c32fc3cdbec6c550a3e65731
robert yin <robert yin>
2017-12-26 07:07:37 +0800

    r

---
+++ run.cmd
@@ -0,0 +1,6 @@
+xcopy C:\Users\84135\AppData\Roaming\npm D:\git\nodeGlobal\npm /E/Y/D
+xcopy C:\Users\84135\AppData\Roaming\npm-cache D:\git\nodeGlobal\npm-cache /E/Y/D
+git add .
+git commit -m "r"
+git push
+cmd
